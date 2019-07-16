f52ff64e3cdc958736ffe02cf21045339fe52e54
unknown <fraj.mohamed97@gmail.com>
2019-07-16 17:22:58 +0100

    first commit

---
+++ src/header-creditcard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Imgcard() {
+    return (
+        <div>
+            <h1 className='credit'>CREDIT CARD</h1>
+            <img className='puce' src='https://static4.depositphotos.com/1003857/292/i/950/depositphotos_2922078-stock-photo-creditcard-chip.jpg'/>  
+        </div>
+    )
+}
+
+export default Imgcard;
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function main() {
+    return (
+        <div className="numbercarholder">
+            <div>
+                <h3 className='number'>7253 3256 7895 1254</h3>
+                <h3 className='n2'>5422</h3>
+                <h2 className='carholder'>CARDHOLDER</h2>
+            </div>
+            <div>
+                <img className="master-card" src="https://www.logolynx.com/images/logolynx/e0/e01dbbb3068c7f5135b90b0ce91445f9.png"/>
+            </div>
+        </div>
+    )
+}
+export default main;
