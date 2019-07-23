683e92acbb3adebe8a3a698d73a4526ef6d70e29
unknown <fraj.mohamed97@gmail.com>
2019-07-23 23:47:09 +0100

    Updates

---
+++ precache-manifest.1ee72a554927dedef338bca355a5448c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583c8e14e0e501042f45100c90d53a02",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "c4349c092afe975e4591",
+    "url": "/credit-card/static/css/main.c8d14063.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/credit-card/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c4349c092afe975e4591",
+    "url": "/credit-card/static/js/main.c6bf1b75.chunk.js"
+  },
+  {
+    "revision": "2a4b22a3002466356f44",
+    "url": "/credit-card/static/js/runtime~main.0914b2aa.js"
+  }
+]);
